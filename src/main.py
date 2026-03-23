6d4781a542449d6ae583f56da0cc5ac21cb6a8a6
Tristan Brideweser <tbridewe@purdue.edu>
2026-03-22 21:22:53 -0400

    feat: fully implemented collision detector; needs validation

--- src/main.py
+++ src/main.py
@@ -1,10 +1,18 @@
 import pybullet as p
 import argparse
-from src.env.env import create_env, reset_env
+from src.env.env import create_env, reset_env, settle_objects
+from src.planner.rrt_connect import RRTConnect
+from src.planner.collision_detector import init_collision_checker, update_obstacles
 
 def main(args):
     client = p.connect(p.GUI)
     create_env()
+    settle_objects()
+    init_collision_checker()
+
+    start = [0, -0.785, 0, -2.356, 0, 1.571, 0.785]
+    goal = [1.0, -0.5, 0.5, -1.5, 0.3, 1.2, 0.5]
+    path = RRTConnect(start, goal)
 
     while True:
         p.stepSimulation()
